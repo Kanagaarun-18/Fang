0300dc21275e6e295acd69326feeda2011493111
KANAGALAKSHMI A <kanagaarun1808@gmail.com>
2026-06-15 15:13:51 +0530

    deploy commit

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ import shutil
 from challenge import get_random_question
 from ai_explainer import explain_message
 
+import os
+
+os.makedirs("uploads", exist_ok=True)
 app = FastAPI(title="FANG")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
